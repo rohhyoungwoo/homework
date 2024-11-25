52bf08e7b809b4df750621ac46a21acb4c8f93ae
rohhyoungwoo <nungma97@naver.com>
2024-11-25 11:57:19 +0900

    241122 22시 30분 java복습후과제제출

---
+++ week01/Printlnfo.java
@@ -0,0 +1,7 @@
+package homework; //패키지 명
+
+public class Printlnfo { //클래스의 중괄호 시작영역
+	public static void main(String[] args) { //main 메소드의 중괄호 시작영역
+		System.out.print("노형우" + 28); //노형우라는 문자에 28인 숫자 값을 덧셈하면 콘솔창에 최종 값이 붙어서 출력
+	} //main 메소드의 중괄호 끝 영역
+} //클래스의 중괄호 끝 영역
