2545f7ee4fc71cf9a7eec13fcfb72ae865f49cd7
rohhyoungwoo <nungma97@naver.com>
2024-12-01 03:32:40 +0900

    241125 1주차 주말과제

---
+++ week01/HelloJava.java
@@ -0,0 +1,7 @@
+package homework; //패키지 명
+
+public class HelloJava { //클래스의 중괄호 시작영역
+	public static void main(String[] args) { //main 메소드의 중괄호 시작영역
+		System.out.print("HelloJava"); //HelloJava라는 값을 콘솔창에 출력한다
+	} //main 메소드의 중괄호 끝 영역
+} //클래스의 중괄호 끝 영역
